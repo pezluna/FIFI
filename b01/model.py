18b953877391f32a30e1951a45783a6857734782
pezluna <pezlunakr@gmail.com>
2024-04-21 11:58:03 +0900

    Update model.py

--- b01/model.py
+++ b01/model.py
@@ -175,9 +175,9 @@ class StatsModel:
         if len(y_train_filtered) == 0:
             print("No data found for the given mode. Check the mode and data.")
             return
-
-        X_train_final = np.array([X_train_filtered[key] for key in ['sPackets', 'rPackets', 'sTotalSize', 'rTotalSize', 'sMinSize', 'rMinSize', 'sMaxSize', 'rMaxSize', 'sAvgSize', 'rAvgSize', 'sVarSize', 'rVarSize', 'sMinInterval', 'rMinInterval', 'sMaxInterval', 'rMaxInterval', 'sAvgInterval', 'rAvgInterval', 'sVarInterval', 'rVarInterval', 'sRatio']]).transpose((1, 2, 0))
-        X_test_final = np.array([X_test_normalized[key] for key in ['sPackets', 'rPackets', 'sTotalSize', 'rTotalSize', 'sMinSize', 'rMinSize', 'sMaxSize', 'rMaxSize', 'sAvgSize', 'rAvgSize', 'sVarSize', 'rVarSize', 'sMinInterval', 'rMinInterval', 'sMaxInterval', 'rMaxInterval', 'sAvgInterval', 'rAvgInterval', 'sVarInterval', 'rVarInterval', 'sRatio']]).transpose((1, 2, 0))
+        
+        X_train_final = np.array(X_train_normalized.values()).transpose()
+        X_test_final = np.array(X_test_normalized.values()).transpose()
 
         self.model.fit(X_train_final, y_train_filtered)
         
