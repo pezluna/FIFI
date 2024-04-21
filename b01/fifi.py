8ca58a4d9b032a8abda1aa4f0fd7d3025f5dbdba
pezluna <pezlunakr@gmail.com>
2024-04-21 21:13:10 +0900

    Update fifi.py

--- b01/fifi.py
+++ b01/fifi.py
@@ -39,35 +39,41 @@ sessions.split_train_test()
 X_train, y_train, X_test, y_test = sessions.get_train_test_data()
 print("Train and test data split completed.")
 
-# Train the model
-print("Training the model...")
-# Packet model
-packet_model = PacketModel(mode=mode, model='cnn')
-packet_X_train, packet_y_train, packet_X_test = packet_model.preprocess(X_train, y_train, X_test)
-packet_y_train = np.array(packet_y_train)
+# Load the model
+print("Model loading...")
 
-# Stats model
-stats_model = StatsModel(mode=mode, model='rf')
-stats_X_train, stats_y_train, stats_X_test = stats_model.preprocess(X_train, y_train, X_test)
-stats_y_train = np.array(stats_y_train)
-
-print(set(packet_y_train))
-print(set(stats_y_train))
+rf_model = StatsModel(mode=mode, model='rf')
+xgb_model = StatsModel(mode=mode, model='xgb')
+cnn_model = PacketModel(mode=mode, model='cnn')
 
 packet_keras_model = KerasClassifier(
-    build_fn=lambda: packet_model.model,
+    build_fn=lambda: cnn_model.model,
     epochs = 50,
     batch_size = 10,
     verbose = 1
 )
 
-# Ensemble classifier
-ensemble = EnsembleClassifier(models={
-    'packet': packet_model.model,
-    'stats': stats_model.model
+ensemble_rf = EnsembleClassifier(models={
+    'packet': cnn_model.model,
+    'stats': rf_model.model
 }, mode=mode)
 
-ensemble.fit(
+ensemble_xgb = EnsembleClassifier(models={
+    'packet': cnn_model.model,
+    'stats': xgb_model.model
+}, mode=mode)
+
+print("Model loaded.")
+
+# Train the model
+print("Training the model...")
+
+packet_X_train, packet_y_train, packet_X_test = cnn_model.preprocess(X_train, y_train, X_test)
+packet_y_train = np.array(packet_y_train)
+stats_X_train, stats_y_train, stats_X_test = rf_model.preprocess(X_train, y_train, X_test)
+stats_y_train = np.array(stats_y_train)
+
+ensemble_rf.fit(
     {
         "packet": packet_X_train,
         "stats": stats_X_train
@@ -75,16 +81,39 @@ ensemble.fit(
     packet_y_train
 )
 
+ensemble_xgb.fit(
+    {
+        "packet": packet_X_train,
+        "stats": stats_X_train
+    },
+    packet_y_train
+)
+
+cnn_model.model.fit(packet_X_train, packet_y_train, epochs=50, batch_size=10, verbose=1)
+rf_model.model.fit(stats_X_train, stats_y_train)
+xgb_model.model.fit(stats_X_train, stats_y_train)
+
 print("Training completed.")
 
 # Evaluate the model
 print("Evaluating the model...")
-predictions = ensemble.predict(
+predictions_ensemble_rf = ensemble_rf.predict(
     {
         "packet": packet_X_test,
         "stats": stats_X_test
     }
 )
+
+predictions_ensemble_xgb = ensemble_xgb.predict(
+    {
+        "packet": packet_X_test,
+        "stats": stats_X_test
+    }
+)
+
+predictions_cnn = cnn_model.model.predict(packet_X_test)
+predictions_rf = rf_model.model.predict(stats_X_test)
+predictions_xgb = xgb_model.model.predict(stats_X_test)
 
 final_y_test = []
 if mode == "fingerprint":
@@ -102,53 +131,57 @@ else:
 
 final_y_test = np.array(final_y_test)
 
-if final_y_test.dtype != predictions.dtype:
-    final_y_test = final_y_test.astype(predictions.dtype)
+if final_y_test.dtype != predictions_rf.dtype:
+    final_y_test = final_y_test.astype(predictions_rf.dtype)
 
-accuracy = accuracy_score(final_y_test, predictions)
+accuracy_rf = accuracy_score(final_y_test, predictions_rf)
+accuracy_xgb = accuracy_score(final_y_test, predictions_xgb)
+accuracy_cnn = accuracy_score(final_y_test, predictions_cnn)
+accuracy_ensemble_rf = accuracy_score(final_y_test, predictions_ensemble_rf)
+accuracy_ensemble_xgb = accuracy_score(final_y_test, predictions_ensemble_xgb)
 
-print("Accuracy: ", accuracy)
+print("Accuracy RF: ", accuracy_rf)
+print("Accuracy XGB: ", accuracy_xgb)
+print("Accuracy CNN: ", accuracy_cnn)
+print("Accuracy Ensemble RF: ", accuracy_ensemble_rf)
+print("Accuracy Ensemble XGB: ", accuracy_ensemble_xgb)
 
-# CNN
-stats_y_pred = stats_model.model.predict(stats_X_test)
-print(set(stats_y_pred))
-print(set(final_y_test))
-
-## Confusion Matrix
+# Save Confusion Matrix
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-cm = confusion_matrix(final_y_test, stats_y_pred)
+cm_rf = confusion_matrix(final_y_test, predictions_rf)
 plt.figure(figsize=(10, 7))
-sns.heatmap(cm, annot=True, fmt='d')
-plt.xlabel('Predicted')
-plt.ylabel('Truth')
-plt.savefig("cnn_confusion_matrix.png")
-print(accuracy_score(final_y_test, stats_y_pred))
-
-# Random Forest
-stats_y_pred = stats_model.model.predict(stats_X_test)
-print(set(stats_y_pred))
-print(set(final_y_test))
-
-## Confusion Matrix
-cm = confusion_matrix(final_y_test, stats_y_pred)
-plt.figure(figsize=(10, 7))
-sns.heatmap(cm, annot=True, fmt='d')
+sns.heatmap(cm_rf, annot=True, fmt='d')
 plt.xlabel('Predicted')
 plt.ylabel('Truth')
 plt.savefig("rf_confusion_matrix.png")
-print(accuracy_score(final_y_test, stats_y_pred))
 
-# Ensemble
-print(set(predictions))
-print(set(final_y_test))
-
-cm = confusion_matrix(final_y_test, predictions)
+cm_xgb = confusion_matrix(final_y_test, predictions_xgb)
 plt.figure(figsize=(10, 7))
-sns.heatmap(cm, annot=True, fmt='d')
+sns.heatmap(cm_xgb, annot=True, fmt='d')
 plt.xlabel('Predicted')
 plt.ylabel('Truth')
-plt.savefig("ensemble_confusion_matrix.png")
-print(accuracy_score(final_y_test, predictions))
+plt.savefig("xgb_confusion_matrix.png")
+
+cm_cnn = confusion_matrix(final_y_test, predictions_cnn)
+plt.figure(figsize=(10, 7))
+sns.heatmap(cm_cnn, annot=True, fmt='d')
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
+plt.savefig("cnn_confusion_matrix.png")
+
+cm_ensemble_rf = confusion_matrix(final_y_test, predictions_ensemble_rf)
+plt.figure(figsize=(10, 7))
+sns.heatmap(cm_ensemble_rf, annot=True, fmt='d')
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
+plt.savefig("ensemble_rf_confusion_matrix.png")
+
+cm_ensemble_xgb = confusion_matrix(final_y_test, predictions_ensemble_xgb)
+plt.figure(figsize=(10, 7))
+sns.heatmap(cm_ensemble_xgb, annot=True, fmt='d')
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
+plt.savefig("ensemble_xgb_confusion_matrix.png")
