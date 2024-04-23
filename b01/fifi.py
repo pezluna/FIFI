107305d25e366f9a5b593b90abfda5b9ed8440ad
pezluna <pezlunakr@gmail.com>
2024-04-23 11:54:06 +0900

    Update fifi.py

--- b01/fifi.py
+++ b01/fifi.py
@@ -179,10 +179,18 @@ final_y_test = np.array(final_y_test)
 if final_y_test.dtype != predictions_rf.dtype:
     final_y_test = final_y_test.astype(predictions_rf.dtype)
 
-print("y_test shape: ", final_y_test.shape)
-
 predictions_cnn = np.argmax(predictions_cnn, axis=1)
 predictions_lstm = np.argmax(predictions_lstm, axis=1)
+
+print(set(final_y_test))
+print(set(predictions_rf))
+print(set(predictions_xgb))
+print(set(predictions_cnn))
+print(set(predictions_lstm))
+print(set(predictions_ensemble_rf_cnn))
+print(set(predictions_ensemble_xgb_cnn))
+print(set(predictions_ensemble_rf_lstm))
+print(set(predictions_ensemble_xgb_lstm))
 
 print("-------------------")
 print("RF")
