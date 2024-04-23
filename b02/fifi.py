5d1ce3140c360c276504e36f0af04a61e61faf2f
pezluna <pezlunakr@gmail.com>
2024-04-23 21:53:10 +0900

    Update fifi.py

--- b02/fifi.py
+++ b02/fifi.py
@@ -60,30 +60,8 @@ print("Train and test data split completed.")
 # Load the model
 print("Model loading...")
 
-rf_model = StatsModel(mode=mode, model='rf')
-xgb_model = StatsModel(mode=mode, model='xgb')
 cnn_model = PacketModel(mode=mode, model='cnn')
 lstm_model = PacketModel(mode=mode, model='lstm')
-
-ensemble_rf_cnn = EnsembleClassifier(models={
-    'packet': cnn_model.model,
-    'stats': rf_model.model
-}, mode=mode)
-
-ensemble_xgb_cnn = EnsembleClassifier(models={
-    'packet': cnn_model.model,
-    'stats': xgb_model.model
-}, mode=mode)
-
-ensemble_rf_lstm = EnsembleClassifier(models={
-    'packet': lstm_model.model,
-    'stats': rf_model.model
-}, mode=mode)
-
-ensemble_xgb_lstm = EnsembleClassifier(models={
-    'packet': lstm_model.model,
-    'stats': xgb_model.model
-}, mode=mode)
 
 print("Model loaded.")
 
@@ -92,82 +70,17 @@ print("Training the model...")
 
 packet_X_train, packet_y_train, packet_X_test = cnn_model.preprocess(X_train, y_train, X_test)
 packet_y_train = np.array(packet_y_train)
-stats_X_train, stats_y_train, stats_X_test = rf_model.preprocess(X_train, y_train, X_test)
-stats_y_train = np.array(stats_y_train)
-
-ensemble_rf_cnn.fit(
-    {
-        "packet": packet_X_train,
-        "stats": stats_X_train
-    },
-    packet_y_train
-)
-
-ensemble_xgb_cnn.fit(
-    {
-        "packet": packet_X_train,
-        "stats": stats_X_train
-    },
-    packet_y_train
-)
-
-ensemble_rf_lstm.fit(
-    {
-        "packet": packet_X_train,
-        "stats": stats_X_train
-    },
-    packet_y_train
-)
-
-ensemble_xgb_lstm.fit(
-    {
-        "packet": packet_X_train,
-        "stats": stats_X_train
-    },
-    packet_y_train
-)
 
 lstm_model.model.fit(packet_X_train, packet_y_train, epochs=50, batch_size=2)
 cnn_model.model.fit(packet_X_train, packet_y_train, epochs=50, batch_size=2)
-rf_model.model.fit(stats_X_train, stats_y_train)
-xgb_model.model.fit(stats_X_train, stats_y_train)
 
 print("Training completed.")
 
 # Evaluate the model
 print("Evaluating the model...")
-predictions_ensemble_rf_cnn = ensemble_rf_cnn.predict(
-    {
-        "packet": packet_X_test,
-        "stats": stats_X_test
-    }
-)
-
-predictions_ensemble_xgb_cnn = ensemble_xgb_cnn.predict(
-    {
-        "packet": packet_X_test,
-        "stats": stats_X_test
-    }
-)
-
-predictions_ensemble_rf_lstm = ensemble_rf_lstm.predict(
-    {
-        "packet": packet_X_test,
-        "stats": stats_X_test
-    }
-)
-
-predictions_ensemble_xgb_lstm = ensemble_xgb_lstm.predict(
-    {
-        "packet": packet_X_test,
-        "stats": stats_X_test
-    }
-)
 
 predictions_lstm = lstm_model.model.predict(packet_X_test)
 predictions_cnn = cnn_model.model.predict(packet_X_test)
-predictions_rf = rf_model.model.predict(stats_X_test)
-predictions_xgb = xgb_model.model.predict(stats_X_test)
 
 final_y_test = []
 if mode == "fingerprint":
@@ -185,35 +98,16 @@ else:
 
 final_y_test = np.array(final_y_test)
 
-if final_y_test.dtype != predictions_rf.dtype:
-    final_y_test = final_y_test.astype(predictions_rf.dtype)
-
 predictions_cnn = np.argmax(predictions_cnn, axis=1)
 predictions_lstm = np.argmax(predictions_lstm, axis=1)
 
+if final_y_test.dtype != predictions_cnn.dtype:
+    final_y_test = final_y_test.astype(predictions_cnn.dtype)
+
 print(set(final_y_test))
-print(set(predictions_rf))
-print(set(predictions_xgb))
+
 print(set(predictions_cnn))
 print(set(predictions_lstm))
-print(set(predictions_ensemble_rf_cnn))
-print(set(predictions_ensemble_xgb_cnn))
-print(set(predictions_ensemble_rf_lstm))
-print(set(predictions_ensemble_xgb_lstm))
-
-print("-------------------")
-print("RF")
-print("Accuracy: ", accuracy_score(final_y_test, predictions_rf))
-print("Precision: ", precision_score(final_y_test, predictions_rf, average='macro'))
-print("Recall: ", recall_score(final_y_test, predictions_rf, average='macro'))
-print("F1: ", f1_score(final_y_test, predictions_rf, average='macro'))
-
-print("-------------------")
-print("XGB")
-print("Accuracy: ", accuracy_score(final_y_test, predictions_xgb))
-print("Precision: ", precision_score(final_y_test, predictions_xgb, average='macro'))
-print("Recall: ", recall_score(final_y_test, predictions_xgb, average='macro'))
-print("F1: ", f1_score(final_y_test, predictions_xgb, average='macro'))
 
 print("-------------------")
 print("CNN")
@@ -229,54 +123,12 @@ print("Precision: ", precision_score(final_y_test, predictions_lstm, average='ma
 print("Recall: ", recall_score(final_y_test, predictions_lstm, average='macro'))
 print("F1: ", f1_score(final_y_test, predictions_lstm, average='macro'))
 
-print("-------------------")
-print("Ensemble RF-CNN")
-print("Accuracy: ", accuracy_score(final_y_test, predictions_ensemble_rf_cnn))
-print("Precision: ", precision_score(final_y_test, predictions_ensemble_rf_cnn, average='macro'))
-print("Recall: ", recall_score(final_y_test, predictions_ensemble_rf_cnn, average='macro'))
-print("F1: ", f1_score(final_y_test, predictions_ensemble_rf_cnn, average='macro'))
-
-print("-------------------")
-print("Ensemble XGB-CNN")
-print("Accuracy: ", accuracy_score(final_y_test, predictions_ensemble_xgb_cnn))
-print("Precision: ", precision_score(final_y_test, predictions_ensemble_xgb_cnn, average='macro'))
-print("Recall: ", recall_score(final_y_test, predictions_ensemble_xgb_cnn, average='macro'))
-print("F1: ", f1_score(final_y_test, predictions_ensemble_xgb_cnn, average='macro'))
-
-print("-------------------")
-print("Ensemble RF-LSTM")
-print("Accuracy: ", accuracy_score(final_y_test, predictions_ensemble_rf_lstm))
-print("Precision: ", precision_score(final_y_test, predictions_ensemble_rf_lstm, average='macro'))
-print("Recall: ", recall_score(final_y_test, predictions_ensemble_rf_lstm, average='macro'))
-print("F1: ", f1_score(final_y_test, predictions_ensemble_rf_lstm, average='macro'))
-
-print("-------------------")
-print("Ensemble XGB-LSTM")
-print("Accuracy: ", accuracy_score(final_y_test, predictions_ensemble_xgb_lstm))
-print("Precision: ", precision_score(final_y_test, predictions_ensemble_xgb_lstm, average='macro'))
-print("Recall: ", recall_score(final_y_test, predictions_ensemble_xgb_lstm, average='macro'))
-print("F1: ", f1_score(final_y_test, predictions_ensemble_xgb_lstm, average='macro'))
-
 print("Evaluation completed.")
 
 # Save Confusion Matrix
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-cm_rf = confusion_matrix(final_y_test, predictions_rf)
-plt.figure(figsize=(10, 7))
-sns.heatmap(cm_rf, annot=True, fmt='d')
-plt.xlabel('Predicted')
-plt.ylabel('Truth')
-plt.savefig("rf_confusion_matrix.png")
-
-cm_xgb = confusion_matrix(final_y_test, predictions_xgb)
-plt.figure(figsize=(10, 7))
-sns.heatmap(cm_xgb, annot=True, fmt='d')
-plt.xlabel('Predicted')
-plt.ylabel('Truth')
-plt.savefig("xgb_confusion_matrix.png")
 
 cm_cnn = confusion_matrix(final_y_test, predictions_cnn)
 plt.figure(figsize=(10, 7))
@@ -291,35 +143,3 @@ sns.heatmap(cm_lstm, annot=True, fmt='d')
 plt.xlabel('Predicted')
 plt.ylabel('Truth')
 plt.savefig("lstm_confusion_matrix.png")
-
-cm_ensemble_rf = confusion_matrix(final_y_test, predictions_ensemble_rf_cnn)
-plt.figure(figsize=(10, 7))
-sns.heatmap(cm_ensemble_rf, annot=True, fmt='d')
-plt.xlabel('Predicted')
-plt.ylabel('Truth')
-plt.savefig("ensemble_rf_cnn_confusion_matrix.png")
-
-cm_ensemble_xgb = confusion_matrix(final_y_test, predictions_ensemble_xgb_cnn)
-plt.figure(figsize=(10, 7))
-sns.heatmap(cm_ensemble_xgb, annot=True, fmt='d')
-plt.xlabel('Predicted')
-plt.ylabel('Truth')
-plt.savefig("ensemble_xgb_cnn_confusion_matrix.png")
-
-cm_ensemble_rf_lstm = confusion_matrix(final_y_test, predictions_ensemble_rf_lstm)
-plt.figure(figsize=(10, 7))
-sns.heatmap(cm_ensemble_rf_lstm, annot=True, fmt='d')
-plt.xlabel('Predicted')
-plt.ylabel('Truth')
-plt.savefig("ensemble_rf_lstm_confusion_matrix.png")
-
-cm_ensemble_xgb_lstm = confusion_matrix(final_y_test, predictions_ensemble_xgb_lstm)
-plt.figure(figsize=(10, 7))
-sns.heatmap(cm_ensemble_xgb_lstm, annot=True, fmt='d')
-plt.xlabel('Predicted')
-plt.ylabel('Truth')
-plt.savefig("ensemble_xgb_lstm_confusion_matrix.png")
-
-print("Confusion matrices saved.")
-
-# Save epoch-loss graph
