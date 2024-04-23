82a7b729002871858b88d9294f06290dbf5cdddd
pezluna <pezlunakr@gmail.com>
2024-04-23 21:59:08 +0900

    Update fifi.py

--- b02/fifi.py
+++ b02/fifi.py
@@ -1,6 +1,6 @@
 import sys
 from session import Sessions
-from model import PacketModel, StatsModel, EnsembleClassifier, embedding_botnet
+from model import PacketModel, embedding_botnet
 import numpy as np
 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
